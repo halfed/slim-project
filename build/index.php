b22a64a7ed6b7763fd8b3912a0d9f734b9d29986
halfed <1half.ed@gmail.com>
2017-08-29 10:09:37 -0600

    adding builds for react

---
+++ build/index.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html>
+<header>
+	<meta charset="UTF-8">
+	<title>The Movie Vault</title>
+
+	<?php
+    include '../partials/header_links.php';
+  ?>
+</header>
+<body>
+
+  <?php
+    include '../partials/header.php';
+  ?>
+
+  <?php
+    include '../partials/breadcrumb.php';
+  ?>
+  
+  <?php
+    include '../partials/nav.php';
+  ?>  
+
+  <div id="app" class="main row movie-container">
+	
+	</div>
+
+  <?php
+    include '../partials/footer.php';
+  ?>
+
+  <?php
+    include '../partials/body_scripts.php';
+  ?>
+<script type="text/javascript" src="app-bundle.js"></script></body>
+</html>
---
+++ build/movie/index.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html>
+<header>
+	<meta charset="UTF-8">
+	<title>The Movie Vault: individual movie</title>
+
+	<?php
+    include '../../partials/header_links.php';
+  ?>
+</header>
+<body>
+	<?php
+    	include '../../partials/header.php';
+  	?>
+
+  	<?php
+    	include '../../partials/breadcrumb.php';
+  	?>
+  
+  	<?php
+    	include '../../partials/nav.php';
+  	?>  
+
+  	<div id="movieApp" class="main row movie-container">
+	
+	</div>
+
+  	<?php
+    	include '../../partials/footer.php';
+  	?>
+
+  	<?php
+    	include '../../partials/body_scripts.php';
+  	?>
+<script type="text/javascript" src="../xampp/htdocs/slim/build/movie-bundle.js"></script></body>
+</html>
