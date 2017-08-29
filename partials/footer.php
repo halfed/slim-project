3878287c033d8c0f318f14425b2a2a9b99cf0e40
halfed <1half.ed@gmail.com>
2017-08-29 10:10:39 -0600

    adding php partials

---
+++ partials/footer.php
@@ -0,0 +1,18 @@
+<footer>
+  <div class="find">
+    Find
+    <ul>
+      <li>New Movies</li>
+      <li>Video Games</li>
+    </ul>
+  </div>
+  <div class="help">
+    Help
+    <ul>
+      <li>Contact Us</li>
+    </ul>
+  </div>
+  <div class="followUs">
+    Follow Us
+  </div>
+</footer>
---
+++ partials/header.php
@@ -0,0 +1,3 @@
+<header>
+	<h1>The Movie Vault</h1>
+</header>
